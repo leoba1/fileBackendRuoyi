b297a993526e3a8fa1499980bb6afa1a0396b1e6
苗木蛤 <2791592045@qq.com>
2024-04-08 23:45:20 +0800

    公告

--- ruoyi-system/src/main/java/com/ruoyi/system/mapper/SysNoticeMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/SysNoticeMapper.java
@@ -5,14 +5,14 @@ import com.ruoyi.system.domain.SysNotice;
 
 /**
  * 通知公告表 数据层
- * 
+ *
  * @author ruoyi
  */
 public interface SysNoticeMapper
 {
     /**
      * 查询公告信息
-     * 
+     *
      * @param noticeId 公告ID
      * @return 公告信息
      */
@@ -20,7 +20,7 @@ public interface SysNoticeMapper
 
     /**
      * 查询公告列表
-     * 
+     *
      * @param notice 公告信息
      * @return 公告集合
      */
@@ -28,15 +28,16 @@ public interface SysNoticeMapper
 
     /**
      * 新增公告
-     * 
+     *
      * @param notice 公告信息
      * @return 结果
      */
     public int insertNotice(SysNotice notice);
+    public int insertNotice2(SysNotice notice);
 
     /**
      * 修改公告
-     * 
+     *
      * @param notice 公告信息
      * @return 结果
      */
@@ -44,7 +45,7 @@ public interface SysNoticeMapper
 
     /**
      * 批量删除公告
-     * 
+     *
      * @param noticeId 公告ID
      * @return 结果
      */
@@ -52,7 +53,7 @@ public interface SysNoticeMapper
 
     /**
      * 批量删除公告信息
-     * 
+     *
      * @param noticeIds 需要删除的公告ID
      * @return 结果
      */
